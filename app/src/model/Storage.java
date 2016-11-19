81dd6c96b0f1697be694a55b1e0b3658c7fe0cec
oTkPoBeHuE <xxoTkPoBeHuExx@gmail.com>
2016-11-20 00:39:07 +0600

    add Products create Transaction

--- app/src/model/Storage.java
+++ app/src/model/Storage.java
@@ -1,4 +1,22 @@
 package model;
 
+import java.util.Map;
+
 public interface Storage {
+
+
+    //// TODO: наверно нужно переделать из интерфейса в абстрактный класс??
+    class Products {
+        //// TODO: rename
+
+        public Products(Product product, int count) {
+            this.product = product;
+            this.count = count;
+        }
+
+        Product product;
+        int count;
+    }
+
+    //Map<String, Products> products;
 }
