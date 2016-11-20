0837a535a5c9bcf910893d29caaf7e8ee573c0db
oTkPoBeHuE <xxoTkPoBeHuExx@gmail.com>
2016-11-20 17:02:10 +0600

    MoneyTest

--- app/src/model/Money.java
+++ app/src/model/Money.java
@@ -4,34 +4,64 @@ import com.sun.xml.internal.bind.v2.TODO;
 
 import java.math.BigDecimal;
 
-public final class  Money {
-    final BigDecimal  amount;
+public final class  Money implements Comparable {
+    private final BigDecimal  amount;
+
+    public Money(String amount){
+        this(new BigDecimal(amount));
+    }
 
     public Money(BigDecimal amount) {
         this.amount = amount;
     }
 
     public Money add(Money other){
-        //// TODO: Реализовать адекватную логику позже  + тесты
         return newMoney(amount.add(other.amount));
     }
 
+    public Money add(String amount){
+        return  this.add(new Money(amount));
+    }
+
     public Money subtract(Money other){
-        ////TODO: Реализовать позже
         return newMoney(amount.subtract(other.amount));
+    }
+
+    public Money subtract(String amount){
+        return  this.subtract(new Money(amount));
     }
     
     private Money newMoney(BigDecimal  amount) {
-        //// TODO: Реализовать позже
         return new Money(amount);
     }
 
     public BigDecimal getAmount() {
-        return amount;
+        return amount.setScale(2, BigDecimal.ROUND_FLOOR);
     }
 
     @Override
     public String toString() {
-        return amount.toString();
+        return getAmount().toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Money money = (Money) o;
+
+        return amount.equals(money.amount);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return amount.hashCode();
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return amount.compareTo(((Money)o).getAmount());
     }
 }
---
+++ app/src/tests/model/MoneyTest.java
@@ -0,0 +1,41 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MoneyTest {
+    private static String NUMBER1 = "99.11";
+    private static String NUMBER2 = "3.11";
+    private static String NUMBER3 = "-3.11";
+
+    private static String NUMBER1_ADD_NUMBER2 = "102.22";
+    private static String NUMBER1_SUB_NUMBER2 = "96.00";
+    private static String NUMBER2_SUB_NUMBER1 = "-96.00";
+    private static String NUMBER1_SUB_NUMBER1 = "0.00";
+
+    private static String NUMBER1_SUB_NUMBER3 = "102.22";
+    private static String NUMBER1_ADD_NUMBER3 = "96.00";
+
+    @Test
+    public void add() throws Exception {
+        assertEquals(NUMBER1_ADD_NUMBER2,  new Money(NUMBER1).add(NUMBER2).toString());
+        assertEquals(NUMBER1_ADD_NUMBER3,  new Money(NUMBER1).add(NUMBER3).toString());
+    }
+
+    @Test
+    public void subtract() throws Exception {
+        assertEquals(NUMBER1_SUB_NUMBER2,  new Money(NUMBER1).subtract(NUMBER2).toString());
+        assertEquals(NUMBER2_SUB_NUMBER1,  new Money(NUMBER2).subtract(NUMBER1).toString());
+        assertEquals(NUMBER1_SUB_NUMBER1,  new Money(NUMBER1).subtract(NUMBER1).toString());
+        assertEquals(NUMBER1_SUB_NUMBER3,  new Money(NUMBER1).subtract(NUMBER3).toString());
+    }
+
+    @Test
+    public void compareTo() throws Exception {
+        assertTrue(new Money(NUMBER1).compareTo(new Money(NUMBER2)) > 0);
+        assertTrue(new Money(NUMBER2).compareTo(new Money(NUMBER1)) < 0);
+        assertTrue(new Money(NUMBER1).compareTo(new Money(NUMBER1)) == 0);
+    }
+
+}
