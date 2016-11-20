9064c0a31c6ef9e56b2fb50540f45d027739fbee
oTkPoBeHuE <xxoTkPoBeHuExx@gmail.com>
2016-11-20 17:10:42 +0600

    Refactor

--- app/src/model/BirdsSorage.java
+++ app/src/model/BirdsSorage.java
@@ -1,4 +1,19 @@
 package model;
 
+import java.util.Map;
+
 public class BirdsSorage implements Storage {
+
+    Map<String, Products> products;
+
+   private class Products {
+        public Products(Product product, int count) {
+            this.product = product;
+            this.count = count;
+        }
+
+        Product product;
+        int count;
+    }
+
 }
--- app/src/model/Storage.java
+++ app/src/model/Storage.java
@@ -1,22 +1,5 @@
 package model;
 
-import java.util.Map;
+interface Storage {
 
-public interface Storage {
-
-
-    //// TODO: наверно нужно переделать из интерфейса в абстрактный класс??
-    class Products {
-        //// TODO: rename
-
-        public Products(Product product, int count) {
-            this.product = product;
-            this.count = count;
-        }
-
-        Product product;
-        int count;
-    }
-
-    //Map<String, Products> products;
 }
