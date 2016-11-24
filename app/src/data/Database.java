e7aec9603bda85f4a9cab189297e28c71ec8955a
oTkPoBeHuE <xxoTkPoBeHuExx@gmail.com>
2016-11-24 23:28:41 +0600

    crate db layer

---
+++ app/src/data/Database.java
@@ -0,0 +1,8 @@
+package data;
+
+/**
+ * Created by oTk on 24.11.2016.
+ */
+public interface Database {
+
+}
---
+++ app/src/data/FileDatabase.java
@@ -0,0 +1,7 @@
+package data;
+
+/**
+ * Created by oTk on 24.11.2016.
+ */
+public class FileDatabase implements Database {
+}
