e6700473ba43a15ee9e5f27b41ee3392c33c349c
oTkPoBeHuE <xxoTkPoBeHuExx@gmail.com>
2016-11-30 02:13:47 +0600

    refactor

--- app/src/data/Database.java
+++ app/src/data/Database.java
@@ -3,15 +3,11 @@ package data;
 import java.util.List;
 import java.util.function.Consumer;
 
-/**
- * Created by oTk on 24.11.2016.
- */
 public interface Database<T> {
     void save(String key, T value);
     T find(String key);
     void delete(String key);
     List<T> findAll();
-
 
     boolean containsKey(String key);
     int size();
